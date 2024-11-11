1c0b493c473cb78d7ad60a5582acd469169cc622
Afoxcute <jwavoabilove16@gmail.com>
2024-11-11 14:56:46 +0000

    fixed CORS issue

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,27 @@ const nextConfig = {
     NEXT_PUBLIC_OKTO_API_KEY: process.env.NEXT_PUBLIC_OKTO_API_KEY,
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ];
+  },
+  images: {
+    domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
 
 export default nextConfig;
